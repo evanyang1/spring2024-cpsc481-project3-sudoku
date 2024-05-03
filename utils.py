9b94277ca1ae36b8ccbc973753b0217c50ae633e
Evan Yang <evanjyang4@gmail.com>
2024-05-02 22:52:59 -0700

    isValidSet

--- utils.py
+++ utils.py
@@ -5,3 +5,6 @@ def getRemainingValues(arr):
     values = {1,2,3,4,5,6,7,8,9}
     return list(values - set(values))
 
+# 1-9 no duplicates
+def isValidSet(arr):
+    return set(arr) == {1, 2, 3, 4, 5, 6, 7, 8, 9} and len(arr) == 9
